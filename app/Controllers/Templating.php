2ab88fb648d1616410f01820f5d34b8c38c1a0fa
1917051045 <77573702+1917051045@users.noreply.github.com>
2021-10-07 13:30:25 +0700

    Praktikum 5

--- app/Controllers/Templating.php
+++ app/Controllers/Templating.php
@@ -3,9 +3,15 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class Templating extends BaseController
 {
+	public function __construct()
+    {
+        $this->UserModel = new UserModel();
+    }
+
 	public function index()
 	{
         $data = [
@@ -16,5 +22,29 @@ class Templating extends BaseController
 		// echo view("v_posts");
 		// echo view("layouts/footer");
         return view("v_admin");
+	}
+
+	public function register()
+	{
+        $data = [
+            'title' => 'Blog - Posts'
+        ];
+		// echo view("layouts/header", ['title' => 'Blog - Posts']);
+		// echo view("layouts/navbar");
+		// echo view("v_posts");
+		// echo view("layouts/footer");
+        return view("v_register");
+	}
+
+	public function saveRegister()
+	{
+        $request = service('request');
+		$data = [
+            'fullname' => $request->getVar('fullname'),
+            'email' => $request->getVar('email'),
+            'password' => $request->getVar('password')
+        ];
+        $this->UserModel->insert($data);
+		return redirect()->to('register');
 	}
 }
